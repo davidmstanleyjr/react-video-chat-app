6b5970a8a21b89850c4d8f5aeafc30f0f6f0160c
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-08-02 11:01:46 -0400

    worked on the videoplayer

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -4,87 +4,100 @@ import Peer from "simple-peer";
 
 const SocketContext = createContext();
 
-const socket = io('http://localhost:5000');
+const socket = io("http://localhost:5000");
 
 const ContextProvider = ({ children }) => {
-    // permission to use the users camera and mic
-    const [stream, setStream] = useState(null);
-    const [me, setMe] = useState('');
-    const [call, setCall] = useState({});
-    const [callAccepted, setCallAccepted] = useState(false);
-    const [callEnded, setCallEnded] = useState(false);
-    const [name, setName] = useState('');
+	// permission to use the users camera and mic
+	const [stream, setStream] = useState(null);
+	const [me, setMe] = useState("");
+	const [call, setCall] = useState({});
+	const [callAccepted, setCallAccepted] = useState(false);
+	const [callEnded, setCallEnded] = useState(false);
+	const [name, setName] = useState("");
 
+	const myVideo = useRef();
+	const userVideo = useRef();
+	const connectionRef = useRef();
 
-    const myVideo = useRef();
-    const userVideo = useRef();
-    const connectionRef = useRef();
+	useEffect(() => {
+		navigator.mediaDevices
+			.getUserMedia({ video: true, audio: true })
+			.then((currentStream) => {
+				setStream(currentStream);
 
+				myVideo.current.srcObject = currentStream;
+			});
+		socket.on("me", (id) => setMe(id));
+		socket.on("calluser", ({ from, name: callerName, signal }) => {
+			setCall({ isReceivedCall: true, from, name: callerName, signal });
+		});
+	}, []);
 
+	const answerCall = () => {
+		setCallAccepted(true);
 
-    useEffect(() => {
-        navigator.mediaDevices.getUserMedia({ video: true, audio:true });
-        .then((currentStream) => {
-            setStream(currentStream);
+		const peer = new Peer({ initiator: false, trickle: false, stream });
 
-            myVideo.current.srcObject = currentStream;
-        });
-        socket.on('me', (id) => setMe(id));
-        socket.on('calluser', ({ from, name: callerName, signal }) => {
-            setCall({ isReceivedCall: true, from, name: callerName, signal })
-        });
-    }, []);
+		peer.on("signal", (data) => {
+			socket.emit("answercall", { signal: data, to: call.from });
+		});
 
-    const answerCall = () => {
-        setCallAccepted(true);
+		peer.on("stream", (currentStream) => {
+			userVideo.current.srcObject = currentStream;
+		});
 
-        const peer = new Peer({ initiator: false, trickle: false, stream });
+		peer.signal(call.signal);
 
-        peer.on('signal', (data) => {
-            socket.emit('answercall', { signal: data, to: call.from });
-        });
+		connectionRef.current = peer;
+	};
 
-        peer.on('stream', (currentStream) => {
-            userVideo.current.srcObject = currentStream;
-        });
+	const calluser = (id) => {
+		const peer = new Peer({ initiator: true, trickle: false, stream });
 
-        peer.signal(call.signal);
+		peer.on("signal", (data) => {
+			socket.emit("callUser", { userToCall: id, signal: data, from: me, name });
+		});
 
-        connectionRef.current = peer;
-    }
+		peer.on("stream", (currentStream) => {
+			userVideo.current.srcObject = currentStream;
+		});
 
-    const calluser = (id) => {
-        const peer = new Peer({ initiator: true, trickle: false, stream });
+		socket.on("callaccepted", (signal) => {
+			setCallAccepted(true);
 
-        peer.on('signal', (data) => {
-            socket.emit('callUser', { userToCall: id, signal: data, from: me, name  });
-        });
+			peer.signal(signal);
+		});
 
-        peer.on('stream', (currentStream) => {
-            userVideo.current.srcObject = currentStream;
-        });
+		connectionRef.current = peer;
+	};
 
-        socket.on('callaccepted', (signal) => {
-            setCallAccepted(true);
+	const leaveCall = () => {
+		setCallEnded(true);
+		connectionRef.current.destroy;
 
-            peer.signal(signal);
-        });
+		window.location.reload();
+	};
 
-        connectionRef.current = peer;
-    }
-
-    const leaveCall = () => {
-        setCallEnded(true);
-        connectionRef.current.destroy;
-
-        window.location.reload();
-    }
-
-    return (
-        <SocketContext.Provider value={{call,callAccepted,myVideo,userVideo,stream,name,setName,callEnded,me,callUser,leaveCall,answerCall,}}>
-            {children}
-        </SocketContext.Provider>
-    )
-}
+	return (
+		<SocketContext.Provider
+			value={{
+				call,
+				callAccepted,
+				myVideo,
+				userVideo,
+				stream,
+				name,
+				setName,
+				callEnded,
+				me,
+				callUser,
+				leaveCall,
+				answerCall,
+			}}
+		>
+			{children}
+		</SocketContext.Provider>
+	);
+};
 
 export { ContextProvider, SocketContext };
--- client/src/components/VideoPlayer.jsx
+++ client/src/components/VideoPlayer.jsx
@@ -1,7 +1,64 @@
 import React from "react";
+import { Grid, Typography, Paper } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+
+import { SocketContext } from "../SocketContext";
+
+const useStyles = makeStyles((theme) => ({
+	video: {
+		width: "550px",
+		[theme.breakpoints.down("xs")]: {
+			width: "300px",
+		},
+	},
+	gridContainer: {
+		justifyContent: "center",
+		[theme.breakpoints.down("xs")]: {
+			flexDirection: "column",
+		},
+	},
+	paper: {
+		padding: "10px",
+		border: "2px solid black",
+		margin: "10px",
+	},
+}));
 
 const VideoPlayer = () => {
-	return <div>VideoPlayer</div>;
+	const classes = useStyles();
+
+	return (
+		<Grid container className={classes.gridContainer}>
+			<Paper classname={classes.paper}>
+				<Grid item xs={12} md={6}>
+					<Typography variant="h5" gutterBottom>
+						Name
+					</Typography>
+					<video
+						playsInLine
+						muted
+						ref={"myVideo"}
+						autoPlay
+						className={classes.video}
+					/>
+				</Grid>
+			</Paper>
+
+			<Paper classname={classes.paper}>
+				<Grid item xs={12} md={6}>
+					<Typography variant="h5" gutterBottom>
+						Name
+					</Typography>
+					<video
+						playsInLine
+						ref={"userVideo"}
+						autoPlay
+						className={classes.video}
+					/>
+				</Grid>
+			</Paper>
+		</Grid>
+	);
 };
 
 export default VideoPlayer;
