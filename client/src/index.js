c70d72e1cde4e9afe18cca35925c9a1c5f85a380
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2021-08-02 07:43:06 -0400

    socket context

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import { ContextProvider } from "./SocketContext";
 import "./style.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+	<ContextProvider>
+		<App />
+	</ContextProvider>,
+	document.getElementById("root")
+);
